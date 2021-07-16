b90b6f2cb3f6587783244263f7d1fff35c6b61dd
meezsue <54166475+meezsue@users.noreply.github.com>
2021-07-16 11:28:56 +0700

    Add files via upload

---
+++ assets/component/footer.php
@@ -0,0 +1,62 @@
+
+    <script src = "<?php echo base_url(); ?>assets/js/jquery-2.2.4.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/metisMenu.min.js"></script>
+
+    <!-- deluxe JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/scripts/init.js"></script>
+
+    <!-- Chart Waypoints counterup -->
+    <script src = "<?php echo base_url(); ?>assets/js/waypoints.min.js"></script>
+
+    <!-- Chart Counterup JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/jquery.counterup.min.js"></script> 
+
+    <!-- Legit Ripple JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/ripple.min.js"></script> 
+
+    <!-- Legit Scrollbar JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/jquery.slimscroll.min.js"></script> 
+
+    <!-- Sweet Alert JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/sweetalert.min.js"></script> 
+    
+    <!-- Bootstrap Growl JavaScript -->
+    <script src = "<?php echo base_url(); ?>assets/js/bootstrap-notify.min.js"></script>
+		
+	<script src = "<?php echo base_url(); ?>assets/js/calendar/moment-with-locales.js"></script>
+	<script src = "<?php echo base_url(); ?>assets/js/calendar/fullcalendar.min.js"></script>
+	<script src = "<?php echo base_url(); ?>assets/js/calendar/bootstrap-material-datetimepicker.js"></script>
+	<script src = "<?php echo base_url(); ?>assets/js/calendar/bootstrap-datetimepicker.min.js"></script>
+	<script src = "<?php echo base_url(); ?>assets/js/scripts/calendar.js"></script>	
+
+    <!-- canvas-to-blob.min.js is only needed if you wish to resize images before upload.
+         This must be loaded before fileinput.min.js -->
+    <script src = "<?php echo base_url(); ?>assets/js/fileinput/plugins/canvas-to-blob.min.js" type = "text/javascript"></script>
+
+    <!-- sortable.min.js is only needed if you wish to sort / rearrange files in initial preview.
+         This must be loaded before fileinput.min.js -->
+    <script src = "<?php echo base_url(); ?>assets/js/fileinput/plugins/sortable.min.js" type = "text/javascript"></script>
+
+    <!-- purify.min.js is only needed if you wish to purify HTML content in your preview for HTML files.
+         This must be loaded before fileinput.min.js -->
+    <script src = "<?php echo base_url(); ?>assets/js/fileinput/plugins/purify.min.js" type = "text/javascript"></script>
+
+    <!-- the main fileinput plugin file -->
+    <script src = "<?php echo base_url(); ?>assets/js/fileinput/fileinput.min.js"></script>
+
+    <!-- optionally if you need a theme like font awesome theme you can include 
+        it as mentioned below -->
+    <script src = "<?php echo base_url(); ?>assets/js/fileinput/themes/fa/theme.js"></script>
+
+    <!--Fileinput Script -->
+    <script src = "<?php echo base_url(); ?>assets/js/fileinput/fileinput.script.js"></script>
+	
+	<script src  = "<?php echo base_url(); ?>assets/js/calendar/bootstrap-material-datetimepicker.js"></script>
+	<script src  = "<?php echo base_url(); ?>assets/js/calendar/bootstrap-datetimepicker.min.js"></script>	
+	<script src  = "<?php echo base_url(); ?>assets/js/datatables.min.js"></script>
+	<script src  = "<?php echo base_url(); ?>assets/js/dataTables.responsive.js"></script>	
---
+++ assets/component/top.php
@@ -0,0 +1,40 @@
+    <!-- Bootstrap Core CSS -->
+    <link href = "<?php echo base_url(); ?>assets/css/bootstrap.css" rel = "stylesheet">
+
+	<link href = "<?php echo base_url(); ?>assets/css/metisMenu.min.css" rel="stylesheet" type="text/css">
+	
+    <!-- Material Design Iconic -->
+    <link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/material-design-iconic-font.min.css">
+
+    <!-- Animate CSS -->
+    <link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/animate.min.css">
+
+    <!-- Legit Ripple CSS -->
+    <link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/ripple.min.css">
+
+    <!-- Hover CSS -->
+    <link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/hover.css">
+	<link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/sweetalert.min.css">
+	<link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/calendar/fullcalendar.min.css">
+	<link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/calendar/fullcalendar.print.css" media="print">	
+	<link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/calendar/bootstrap-material-datetimepicker.css">
+	<link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/calendar/bootstrap-datetimepicker.min.css">		
+    <!-- Social Buttons CSS -->
+    <link rel = "stylesheet" href = "<?php echo base_url(); ?>assets/css/social-buttons.css">
+	
+	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/datatables.min.css" />
+	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/dataTables.responsive.css" />
+	
+    <!-- Deluxe Custom CSS -->
+    <link href = "<?php echo base_url(); ?>assets/css/deluxe-admin.css" rel = "stylesheet">
+	 	
+	<!-- jQuery -->
+    <script src = "<?php echo base_url(); ?>assets/js/jquery-2.2.4.min.js"></script>	
+    <!-- Legit Scrollbar CSS -->
+    <!-- <link rel = "stylesheet" href = "css/jquery.mCustomScrollbar.min.css"> -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src = "https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src = "https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
